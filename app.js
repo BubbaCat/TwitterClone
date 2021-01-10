529b4508775133227ec1b5bed7f4dcc522c2f675
Evgeniy <evgeniy.sapov@gmail.com>
2021-01-10 10:46:02 +0500

    add schemas for mongoDB

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const middleware = require("./middleware");
 const path = require("path");
 const bodyParser = require('body-parser');
 const mongoose = require("./database");
+const session = require("express-session");
 //Routes
 const loginRoute = require("./routes/loginRoutes");
 const registerRoute = require("./routes/registerRoutes");
 
 //Config stuff
 const app = express();
-const port = 3005;
+const port = 3000;
 //set up the server
 const server = app.listen(port, () => {
 	console.log("fine");
@@ -21,7 +22,11 @@ app.set("views", "views");
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,"public")));
-
+app.use(session({
+	secret:"dasdsaftq3edsxcfgedsgfa asdsadsad",
+	resave:true,
+	saveUninitialized:false,
+}));
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
 
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -1,20 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get("/",(req,res,next)=>{
-    res.status(200).render('register');
+const User = require("../schemas/userSchema");
+const bcrypt = require("bcrypt");
+router.get("/", (req, res, next) => {
+  res.status(200).render("register");
 });
 
-router.post("/",(req,res,next)=>{
-   let {firstName,lastName,login,email,password,passwordConf} = req.body;
-   if(firstName && lastName && login && email && password && passwordConf && passwordConf==password){
-
-   }
-   else{
-    req.body.errorMessage = "Make sure all fields has a valid value";
-    res.status(200).render('register',req.body);
-   }
-    res.status(200).render('register');
+router.post("/", async (req, res, next) => {
+  let { firstName, lastName, login, email, password, passwordConf } = req.body;
+  if (
+	firstName &&
+	lastName &&
+	login &&
+	email &&
+	password &&
+	passwordConf &&
+	passwordConf == password
+  ) {
+	let user = await User.findOne({
+	  $or: [{ username: login }, { email: email }],
+	}).catch(error=>console.log(error));
+	if(user==null){
+		let data = req.body;
+		data.password= await bcrypt.hash(password,10);
+		User.create(data).then(data=>{
+			req.session.user = user;
+			return res.redirect("/");
+		});
+	}else {
+		if(email==user.email){
+			req.body.errorMessage = "email already in use";
+			res.status(200).render("register", req.body);
+		}
+	}
+  } else {
+	req.body.errorMessage = "Make sure all fields has a valid value";
+	res.status(200).render("register", req.body);
+  }
+  res.status(200).render("register");
 });
 
 module.exports = router;
